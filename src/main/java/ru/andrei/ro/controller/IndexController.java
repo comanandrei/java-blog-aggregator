396d6650991e0c831f4c37d2e9588868b10768b3
Andrei <andrei.coman.88@gmail.com>
2016-12-30 17:12:33 +0200

    Create entities

---
+++ src/main/java/ru/andrei/ro/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ru.andrei.ro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
